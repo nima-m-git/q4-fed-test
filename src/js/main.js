02c194c62a2eaf90ab7e35ed0426e85de964e861
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2020-12-18 13:49:23 -0500

    test complete

--- src/js/main.js
+++ src/js/main.js
@@ -5,12 +5,13 @@ var reportsWidget = {
             '{{#.}}' +
                 '<article class="reports_item">' +
                     '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'
+                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>' +
                     '</a>' +
                     '<footer class="reports_docs">' +
                         '{{#documents}}' +
                             '<h3 class="reports_title">' +
-                                '<a href="{{url}}" target="_blank">{{title}}</a>' +
+                                '<a href="{{url}}" target="_blank">{{title}} </a>' +
+                                '<span class="file_info">({{file_size}} {{file_type}})</span>' +
                             '</h3>' +
                         '{{/documents}}' +
                     '</footer>' +
@@ -24,8 +25,7 @@ var reportsWidget = {
     },
 
     renderReports: function(reports) {
-        var inst = this,
-            options = inst.options;
+        var options = this.options;
 
         $(options.containerSelector).html(Mustache.render(options.template, reports));
     }
